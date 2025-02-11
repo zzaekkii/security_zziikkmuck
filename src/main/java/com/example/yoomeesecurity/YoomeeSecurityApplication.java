e4049afabd9a1d6dc29a02571e26fd8029498902
zakie <realzzaekkii@gmail.com>
2025-02-11 14:54:20 +0900

    first commit

---
+++ src/main/java/com/example/yoomeesecurity/YoomeeSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.yoomeesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoomeeSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoomeeSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yoomeesecurity/YoomeeSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yoomeesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoomeeSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
