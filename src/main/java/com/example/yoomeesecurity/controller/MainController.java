ac31e6940f3e94f41d09cef838aafd8f2977db80
zakie <realzzaekkii@gmail.com>
2025-02-12 19:34:18 +0900

    Feat: 사용자 세션 정보에 따른 화면 표시

--- src/main/java/com/example/yoomeesecurity/controller/MainController.java
+++ src/main/java/com/example/yoomeesecurity/controller/MainController.java
@@ -1,13 +1,27 @@
 package com.example.yoomeesecurity.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 로그인한 사용자에게 띄워줄 정보 추가.
+        if (authentication != null && authentication.isAuthenticated() && !"anonymousUser".equals(authentication.getName())) {
+            String userId = authentication.getName();
+            String role = authentication.getAuthorities().iterator().next().getAuthority(); // 하나만 존재함
+
+            model.addAttribute("userId", userId);
+            model.addAttribute("role", role);
+        }
 
         return "main";
     }
